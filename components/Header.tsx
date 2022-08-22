f1bfeb155fb05996dc9a6537a0522a64defcf7a5
dan-michell <dan.michell1000@gmail.com>
2022-08-22 19:38:40 +0100

    navbar progress

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-import { BiSun, BiMoon } from "react-icons/bi";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
+import { GoTerminal } from "react-icons/go";
+import { FiMenu } from "react-icons/fi";
 import { render } from "react-dom";
+import { MotionConfig } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 type Props = {};
 
 const Header: React.FC<Props> = (props: Props) => {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
   useEffect(() => {
     setMounted(true);
@@ -22,30 +27,60 @@ const Header: React.FC<Props> = (props: Props) => {
 
     if (currentTheme === "dark") {
       return (
-        <BiSun
-          size={48}
-          className=" hover:scale-105 transition-all duration-200"
-          onClick={() => {
-            setTheme("light");
-          }}
-        />
+        <motion.div
+          animate={{ rotate: 180 }}
+          transition={{ duration: 0.3, type: "spring", stiffness: 100 }}
+        >
+          <MdOutlineLightMode
+            size={28}
+            onClick={() => {
+              setTheme("light");
+            }}
+          />
+        </motion.div>
       );
     } else {
       return (
-        <BiMoon
-          size={48}
-          className=" hover:scale-105 transition-all duration-200"
-          onClick={() => {
-            setTheme("dark");
-          }}
-        />
+        <motion.div
+          transition={{ duration: 0.3, type: "spring", stiffness: 100 }}
+        >
+          <MdOutlineDarkMode
+            size={28}
+            onClick={() => {
+              setTheme("dark");
+            }}
+          />
+        </motion.div>
       );
     }
   };
   return (
-    <div className="w-[80%] border-2 flex justify-between">
-      <h1>Header</h1>
-      {renderThemeChanger()}
+    <div className="w-[80%] my-2 max-w-2xl flex justify-between items-center">
+      <div className="flex items-center">
+        <GoTerminal size={32} />
+        <span className="text-lg ml-2 font-mono">dan_michell</span>
+      </div>
+      <div className="flex items-center gap-3">
+        <p>About</p>
+        <p>Work</p>
+        <p>Source</p>
+        <div className="p-1 border-[1px] rounded-xl">
+          {renderThemeChanger()}
+        </div>
+        {/* <div
+          className="p-1 border-[1px] rounded-xl"
+          onClick={() => {
+            setDropdown(!dropdown);
+          }}
+        >
+          <FiMenu size={28} />
+          <div
+            className={`absolute right-[60px] h-10 w-52 ${
+              dropdown ? "border-2" : "hidden"
+            }`}
+          ></div>
+        </div> */}
+      </div>
     </div>
   );
 };
