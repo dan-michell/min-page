dd3f243a382e811e17a13c288af17b86f7c229e9
dan-michell <dan.michell1000@gmail.com>
2024-05-22 22:36:55 +0100

    Picking up project

--- components/About.tsx
+++ components/About.tsx
@@ -3,21 +3,23 @@ import React from "react";
 type Props = {};
 
 const About: React.FC<Props> = (props: Props) => {
-  return (
-    <div id="about" className="w-[90%] sm:w-auto mt-6 flex flex-col">
-      {/* <h1 className="sm:text-2xl text-xl font-bold border-b-2 w-fit mb-2">
+	return (
+		<div id="about" className="w-[90%] sm:w-auto mt-6 flex flex-col">
+			{/* <h1 className="sm:text-2xl text-xl font-bold border-b-2 w-fit mb-2">
         About
       </h1> */}
-      <p className="font-light text-justify">
-        Thank you for taking the time to look at my website! I'm Dan, a junior
-        fullstack software engineer based near London. Having recently entered
-        the world of Software Development from a background in Mechanical
-        Engineering I have naturally taken to the problem solving and logical
-        aspect that coding offers.
-        {/* Add profile pic */}
-      </p>
-    </div>
-  );
+			<p className="font-light text-justify">
+				Thank you for taking the time to look at my website! I'm Dan, a software engineer
+				based in London. I like to keep things simple; see below for some more info about me
+				or check out the{" "}
+				<i>
+					<b>Feed</b>
+				</i>{" "}
+				page to see what i'm up to.
+				{/* Add profile pic */}
+			</p>
+		</div>
+	);
 };
 
 export default About;
--- components/Header.tsx
+++ components/Header.tsx
@@ -10,139 +10,135 @@ import NoScrollLink from "./NoScrollLink";
 type Props = {};
 
 const Header: React.FC<Props> = (props: Props) => {
-  const { systemTheme, theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-  const [dropdown, setDropdown] = useState(false);
+	const { systemTheme, theme, setTheme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+	const [dropdown, setDropdown] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+	useEffect(() => {
+		setMounted(true);
+	}, []);
 
-  const renderThemeChanger = () => {
-    if (!mounted) {
-      return null;
-    }
+	const renderThemeChanger = () => {
+		if (!mounted) {
+			return null;
+		}
 
-    const currentTheme = theme === "system" ? systemTheme : theme;
+		const currentTheme = theme === "system" ? systemTheme : theme;
 
-    if (currentTheme === "dark") {
-      return (
-        <motion.div
-          animate={{ rotate: 180 }}
-          transition={{ duration: 0.3, type: "spring", stiffness: 100 }}
-        >
-          <MdOutlineLightMode
-            size={22}
-            onClick={() => {
-              setTheme("light");
-            }}
-          />
-        </motion.div>
-      );
-    } else {
-      return (
-        <motion.div
-          transition={{ duration: 0.3, type: "spring", stiffness: 100 }}
-        >
-          <MdOutlineDarkMode
-            size={22}
-            onClick={() => {
-              setTheme("dark");
-            }}
-          />
-        </motion.div>
-      );
-    }
-  };
-  return (
-    <div className="sm:w-[80%] w-full px-4 sm:px-0 my-4 max-w-2xl flex justify-between items-center">
-      <NoScrollLink href="/">
-        <button>
-          <div className="flex items-center">
-            <GoTerminal size={32} />
-            <span className="text-xl ml-2 font-mono font-semibold">
-              dan_michell
-            </span>
-          </div>
-        </button>
-      </NoScrollLink>
+		if (currentTheme === "dark") {
+			return (
+				<motion.div
+					animate={{ rotate: 180 }}
+					transition={{ duration: 0.3, type: "spring", stiffness: 100 }}
+				>
+					<MdOutlineLightMode
+						size={22}
+						onClick={() => {
+							setTheme("light");
+						}}
+					/>
+				</motion.div>
+			);
+		} else {
+			return (
+				<motion.div transition={{ duration: 0.3, type: "spring", stiffness: 100 }}>
+					<MdOutlineDarkMode
+						size={22}
+						onClick={() => {
+							setTheme("dark");
+						}}
+					/>
+				</motion.div>
+			);
+		}
+	};
+	return (
+		<div className="sm:w-[80%] w-full px-4 sm:px-0 my-4 max-w-2xl flex justify-between items-center">
+			<NoScrollLink href="/">
+				<button>
+					<div className="flex items-center">
+						<GoTerminal size={32} />
+						<span className="text-xl ml-2 font-mono font-semibold">dan_michell</span>
+					</div>
+				</button>
+			</NoScrollLink>
 
-      <div className="flex items-center gap-4">
-        <div className="sm:flex items-center gap-4 hidden">
-          <button className="hover:scale-110 transition-all duration-200">
-            <a href="#blog">Blog</a>
-          </button>
-          <button className="hover:scale-110 transition-all duration-200">
-            <a href="#work">Work</a>
-          </button>
-          <button className="hover:scale-110 transition-all duration-200">
-            <a
-              className="flex gap-1 items-center"
-              href="https://github.com/dan-michell/min-page"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Source
-              <AiFillGithub size={22} />
-            </a>
-          </button>
-        </div>
-        <div className="flex justify-center sm:hidden">
-          <div>
-            <div className="relative">
-              <button
-                onClick={() => {
-                  setDropdown(!dropdown);
-                }}
-                // onBlur={() => {
-                //   setDropdown(false);
-                // }}
-                className="p-[11px] border-[1px] dark:border-gray-700 rounded-xl hover:dark:bg-gray-900 hover:bg-gray-100"
-              >
-                <FiMenu size={16} />
-              </button>
-              <ul
-                className={`absolute ${
-                  dropdown ? "" : "hidden"
-                } py-2 border-[1px] bg-black dark:border-gray-700 list-none rounded-lg shadow-lg mt-2 left-[-90px] w-48`}
-              >
-                <li>
-                  <button className="text-sm  px-6 w-full hover:scale-110 transition-all duration-200">
-                    <a href="#blog">Blog</a>
-                  </button>
-                </li>
-                <li>
-                  <button className="text-sm px-6 mt-3 w-full hover:scale-110 transition-all duration-200">
-                    <a href="#work">Work</a>
-                  </button>
-                </li>
-                <li>
-                  <button className="text-sm px-6 mt-3 w-full flex justify-center hover:scale-110 transition-all duration-200">
-                    <a
-                      className="flex gap-1 items-center"
-                      href="https://github.com/dan-michell/min-page"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Source
-                      <AiFillGithub size={22} />
-                    </a>
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <button
-          className={
-            "p-2 rounded-xl bg-slate-800 text-gray-200 dark:bg-orange-300 dark:text-gray-900 hover:bg-slate-900 hover:dark:bg-orange-200"
-          }
-        >
-          {renderThemeChanger()}
-        </button>
-      </div>
-    </div>
-  );
+			<div className="flex items-center gap-4">
+				<div className="sm:flex items-center gap-4 hidden">
+					<button className="hover:scale-110 transition-all duration-200">
+						<a href="#blog">Blog</a>
+					</button>
+					<button className="hover:scale-110 transition-all duration-200">
+						<a href="#work">Work</a>
+					</button>
+					<button className="hover:scale-110 transition-all duration-200">
+						<a
+							className="flex gap-1 items-center"
+							href="https://github.com/dan-michell/min-page"
+							target="_blank"
+							rel="noreferrer"
+						>
+							Source
+							<AiFillGithub size={22} />
+						</a>
+					</button>
+				</div>
+				<div className="flex justify-center sm:hidden">
+					<div>
+						<div className="relative">
+							<button
+								onClick={() => {
+									setDropdown(!dropdown);
+								}}
+								// onBlur={() => {
+								//   setDropdown(false);
+								// }}
+								className="p-[11px] border-[1px] dark:border-gray-700 rounded-xl hover:dark:bg-gray-900 hover:bg-gray-100"
+							>
+								<FiMenu size={16} />
+							</button>
+							<ul
+								className={`absolute ${
+									dropdown ? "" : "hidden"
+								} py-2 border-[1px] bg-black dark:border-gray-700 list-none rounded-lg shadow-lg mt-2 left-[-90px] w-48`}
+							>
+								<li>
+									<button className="text-sm  px-6 w-full hover:scale-110 transition-all duration-200">
+										<a href="#blog">Blog</a>
+									</button>
+								</li>
+								<li>
+									<button className="text-sm px-6 mt-3 w-full hover:scale-110 transition-all duration-200">
+										<a href="#work">Work</a>
+									</button>
+								</li>
+								<li>
+									<button className="text-sm px-6 mt-3 w-full flex justify-center hover:scale-110 transition-all duration-200">
+										<a
+											className="flex gap-1 items-center"
+											href="https://github.com/dan-michell/min-page"
+											target="_blank"
+											rel="noreferrer"
+										>
+											Source
+											<AiFillGithub size={22} />
+										</a>
+									</button>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<button
+					className={
+						"p-2 rounded-xl bg-slate-800 text-gray-200 dark:bg-orange-300 dark:text-gray-900 hover:bg-slate-900 hover:dark:bg-orange-200"
+					}
+				>
+					{renderThemeChanger()}
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
