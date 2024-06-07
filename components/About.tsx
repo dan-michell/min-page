15e4f91507d865ba257e594ec4de386287ae8014
dan-michell <dan.michell1000@gmail.com>
2024-06-07 15:34:07 +0100

    Sorted mobile welcome sizing and profile image location

--- components/About.tsx
+++ components/About.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Image from "next/image";
+
+import profileImage from "../public/profile-square.jpg";
 
 type Props = {};
 
 const About: React.FC<Props> = (props: Props) => {
 	return (
-		<div id="about" className="w-[90%] sm:w-full mt-12 flex flex-col">
+		<div id="about" className="w-[90%] sm:w-full mt-12 flex items-center gap-6">
+			<Image
+				src={profileImage}
+				sizes="100vh"
+				alt="Picture of the author"
+				className="aspect-square max-w-[124px] rounded-full border-[1px] border-lightish-light dark:border-darkish-light"
+			/>
 			<p className="font-light text-justify">
 				Thank you for taking the time to look at my website! I'm a software engineer based
 				in London. I like to keep things simple; see below for some more info about me or
--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -9,15 +9,15 @@ type Props = {};
 const Welcome: React.FC<Props> = (props: Props) => {
 	return (
 		<div className="mt-8 whitespace-nowrap">
-			<h1 className="font-semibold relative sm:text-9xl text-[7rem] leading-[1]">
+			<h1 className="font-semibold relative sm:text-9xl text-[6rem] leading-[1]">
 				<div className="flex">
 					Hello!
-					<Image
+					{/* <Image
 						src={profileImage}
 						sizes="100vh"
 						alt="Picture of the author"
-						className="aspect-square max-w-[112px] ml-3 rounded-full self-center border-[1px] border-lightish-light dark:border-darkish-light"
-					/>
+						className="aspect-square max-w-[112px] ml-3 rounded-full self-end border-[1px] border-lightish-light dark:border-darkish-light"
+					/> */}
 				</div>
 				I'm Dan.
 			</h1>
